cea11a42d25a83c72997cadded334ef88c645a3b
Benign X <1341398182@qq.com>
2024-07-18 12:31:08 +0800

    😁(tuning): tuning support
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+#[derive(Copy, Clone, Debug)]
+#[repr(u8)]
+#[derive(PartialEq)]
+enum Tuning {
+    C = 1,
+    D = 2,
+    E = 3,
+    F = 4,
+    G = 5,
+    A = 6,
+    B = 7,
+}
+
+impl From<u8> for Tuning {
+    fn from(value: u8) -> Self {
+        match value {
+            1 => Tuning::C,
+            2 => Tuning::D,
+            3 => Tuning::E,
+            4 => Tuning::F,
+            5 => Tuning::G,
+            6 => Tuning::A,
+            7 => Tuning::B,
+            _ => panic!("Invalid value"),
+        }
+    }
+}
+
+impl Tuning {
+    pub fn modulation(&self, degree: i8) -> Tuning {
+        let value = ((*self as i8 - 1 + 7 + degree) % 7 + 1) as u8;
+        Tuning::from(value)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Tuning::C;
+    let b = a.modulation(-1);
+    println!("{:?}", b);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_modulation() {
+        assert_eq!(Tuning::C.modulation(2), Tuning::E);
+        assert_eq!(Tuning::C.modulation(-2), Tuning::A);
+        assert_eq!(Tuning::C.modulation(7), Tuning::C);
+        assert_eq!(Tuning::C.modulation(-7), Tuning::C);
+    }
 }
