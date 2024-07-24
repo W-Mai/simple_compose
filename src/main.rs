7fd1d94ed3ad83e2eea3300bdeb4ef6a06209633
Benign X <1341398182@qq.com>
2024-07-25 00:47:59 +0800

    😁(main): refactor codes to fit new midi_player
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
+use rand::prelude::*;
 use std::thread::sleep;
 use std::time::Duration;
-use rand::prelude::*;
-use midir::MidiOutput;
 
 use simple_compose::*;
 
@@ -20,7 +19,7 @@ fn generate_duration() -> Vec<f32> {
             5 => 1.0,
             6 => 2.0,
             7 => 4.0,
-            _ => { unreachable!(); }
+            _ => unreachable!(),
         };
         if duration_sum + duration > 4.0 {
             break;
@@ -57,75 +56,29 @@ fn main() {
 
     let mut rng = thread_rng();
 
-    let midi_out = MidiOutput::new("My Test Output").unwrap();
-
-    // Get an output port (read from console if multiple are available)
-    let out_ports = midi_out.ports();
-
-    let need_play = out_ports.len() > 0;
-
-    let out_port = if need_play {
-        Some(match out_ports.len() {
-            1 => {
-                println!(
-                    "Choosing the only available output port: {}",
-                    midi_out.port_name(&out_ports[0]).unwrap()
-                );
-                &out_ports[0]
-            }
-            _ => {
-                println!("\nAvailable output ports:");
-                for (i, p) in out_ports.iter().enumerate() {
-                    println!("{}: {}", i, midi_out.port_name(p).unwrap());
-                }
-                print!("Please select output port: ");
-                let mut input = String::new();
-                out_ports
-                    .get(input.trim().parse::<usize>().unwrap())
-                    .ok_or("invalid output port selected").unwrap()
-            }
-        })
-    } else { None };
-
-
-    println!("\nOpening connection");
-    let mut conn_out = if need_play { Some(midi_out.connect(out_port.unwrap(), "midir-test").unwrap()) } else { None };
-    println!("Connection open. Listen!");
-    // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
-    let mut play_note = |note: u8, duration: u64| {
-        const NOTE_ON_MSG: u8 = 0x90;
-        const NOTE_OFF_MSG: u8 = 0x80;
-        const VELOCITY: u8 = 0x64;
-        // We're ignoring errors in here
-        if let Some(conn_out) = conn_out.as_mut() {
-            let _ = conn_out.send(&[NOTE_ON_MSG | 0, note, VELOCITY]);
-            let _ = conn_out.send(&[NOTE_ON_MSG | 1, note + 4, VELOCITY]);
-            let _ = conn_out.send(&[NOTE_ON_MSG | 2, note + 7, VELOCITY]);
-            sleep(Duration::from_millis(duration * 80));
-            let _ = conn_out.send(&[NOTE_OFF_MSG | 0, note, VELOCITY]);
-            let _ = conn_out.send(&[NOTE_OFF_MSG | 1, note + 4, VELOCITY]);
-            let _ = conn_out.send(&[NOTE_OFF_MSG | 2, note + 7, VELOCITY]);
-        }
-    };
-    {
-        for chord in chords {
-            let notes = chord.breakdown(4);
-            let durations = generate_duration();
-            for duration in durations {
-                let note = notes.choose(&mut rng).unwrap().clone();
-                let note = note.with_duration(duration);
-                print!("{}[{}] ", note, duration);
-
-                play_note(note.chord as u8 + (note.octave + 1) * 12 - 1, (duration * 8.0) as u64)
-            }
-            println!("|");
-        }
+    let mut midi_player = MidiPlayer::new("Simple Compose");
+    let ports = midi_player.list_ports();
+    let need_play = ports.len() > 0;
+    if need_play {
+        midi_player.select_port(0).unwrap();
+        midi_player.connect("Simple Compose Port 0").unwrap();
     }
 
-    println!("\nClosing connection");
-    // This is optional, the connection would automatically be closed as soon as it goes out of scope
-    if let Some(conn_out) = conn_out {
-        conn_out.close();
+    for chord in chords {
+        let notes = chord.breakdown(4);
+        let durations = generate_duration();
+        for duration in durations {
+            let note = notes.choose(&mut rng).unwrap().clone();
+            let note = note.with_duration(duration);
+            print!("{}[{}] ", note, duration);
+
+            let note = note.chord as u8 + (note.octave + 1) * 12 - 1;
+            midi_player.play(note);
+            sleep(Duration::from_millis((duration * 80.0 * 8.0) as u64));
+            midi_player.stop(note);
+        }
+        println!("|");
     }
-    println!("Connection closed");
+
+    midi_player.close();
 }
