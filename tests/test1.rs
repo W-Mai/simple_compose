8c0eee1802c2cecd8d36a89909dd9ff98fc34e29
Benign X <1341398182@qq.com>
2025-02-06 19:35:02 +0800

    ✨(tests): update duration and measurement tests
    Updated the duration tests to utilize a new struct method and corrected string representation for duration. Additionally, adjusted the loop in the measurement test to start from 1 for better coverage.

--- tests/test1.rs
+++ tests/test1.rs
@@ -19,19 +19,25 @@ mod tests {
 
     #[test]
     fn test_duration_1() {
-        assert_eq!(Duration::Whole + Duration::Half, 1.5);
-        assert_eq!(<Duration as Into<f32>>::into(Duration::Half), 0.5);
-        assert_eq!(Duration::from(0.5), Duration::Half);
+        assert_eq!(
+            Duration::new(DurationBase::Whole) + Duration::new(DurationBase::Half),
+            1.5
+        );
+        assert_eq!(
+            <Duration as Into<f32>>::into(Duration::new(DurationBase::Half)),
+            0.5
+        );
+        assert_eq!(Duration::from(0.5).base, DurationBase::Half);
     }
 
     #[test]
     fn test_duration_2() {
-        assert_eq!(Duration::Half.to_string(), "½");
+        assert_eq!(Duration::new(DurationBase::Half).to_string(), "𝅗𝅥");
     }
 
     #[test]
     fn test_measure_1() {
-        for i in 0..16 {
+        for i in 1..16 {
             for _ in 0..8 {
                 do_a_measure_test(i);
             }
