2df6e0a203e9f9c91b12080f06d6806ea74b7de4
Benign X <1341398182@qq.com>
2025-03-13 05:38:47 +0800

    ✨(main): refactor main function for modular chord generation
    Refactored the `main` function to use an array of pitch classes for generating chords dynamically. This enhancement simplifies the code and allows for easier modification of the chord sequences by iterating through the defined pitch classes. Additionally, the tempo and time signature were updated for improved musical composition.

--- src/main.rs
+++ src/main.rs
@@ -15,24 +15,50 @@ const BEAT_TYPE: DurationBase = DurationBase::Quarter;
 ///              common_chord            breakdown
 /// ```
 fn main() {
-    let chords = degrees!(PitchClass::F
-        => 1 1 4 5 1 4 1
-    );
+    // Declares the modulation mode
+    let pitch_classes = [
+        PitchClass::C,
+        PitchClass::F,
+        PitchClass::D,
+        PitchClass::G,
+        PitchClass::C,
+    ];
 
+    // Declares a score with 2 tracks
+    // and a tempo of Vivace
+    // and a time signature of 3/4
     let mut score = Score::<2>::new()
-        .with_tempo(Tempo::Allegro)
+        .with_tempo(Tempo::Vivace)
         .with_time_signature(BEAT, BEAT_TYPE);
+
+    // Get the duration generator from the score. Currently, it is set to 3/4.
+    // It means each beat is a quarter note.
     let dg = score.duration_generator();
 
-    (0..chords.len()).for_each(|i| {
-        score.new_measures(|m| {
-            m[0].chord(chords[i].clone());
-            m[1] = duration_utils::generate_one_measure(&dg, chords[i].clone(), BEAT);
-        })
-    });
+    // Generate the measures for each pitch class
+    for pitch_class in pitch_classes {
+        // Generate the chord for each pitch class
+        // Current it will generate a chord sequence of
+        // 1 1 4 5 1 4 1 degrees
+        let chords = degrees!(pitch_class
+            => 1 1 4 5 1 4 1
+        );
 
+        // Generate the measures for each chord
+        (0..chords.len()).for_each(|i| {
+            score.new_measures(|m| {
+                // Set the first track to play the chord
+                m[0].chord(chords[i].clone());
+                // Set the second track to play the notes of the chord randomly
+                m[1] = duration_utils::generate_one_measure(&dg, chords[i].clone(), BEAT);
+            })
+        });
+    }
+
+    // Instantiate the MidiPlayer
     let mut midi_player = MidiPlayer::new("Simple Compose");
+    // Play the score
     midi_player.play_score(score).unwrap();
-
+    // Close the MidiPlayer
     midi_player.close();
 }
